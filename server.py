82a60cf7bc0c07061286f2e7c5f7a21a2fc6a2fe
Denis Silin <Silin.dns@gmail.com>
2021-06-09 21:05:16 +0300

    added hiro example

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 from coordinates import longitude_now, latitude_now
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 
 def index():
     title = "AR vision"
