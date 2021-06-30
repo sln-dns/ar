7197cbf50e2cef574cd537ec2038b28e3a268062
Denis Silin <Silin.dns@gmail.com>
2021-07-01 02:20:30 +0300

    added login, change bootstrap scheme

--- webapp/config.py
+++ webapp/config.py
@@ -1,4 +1,5 @@
 import os 
+from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,3 +7,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'ar.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = "45674567"
+
+REMEMBER_COOKIE_DURATION = timedelta(days=1)
